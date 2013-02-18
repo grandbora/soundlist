9f4db46d50fb7d8013d35c46c785eed0b4e137bf
grandbora <bora.tunca@yahoo.com>
2013-02-18 22:19:57 +0100

    bookmarklet sc loaded

--- public/js/bookmarklet.js
+++ public/js/bookmarklet.js
@@ -1,1 +1,13 @@
-alert('asdas');
+function start () {
+  debugger
+}
+
+(function(d){
+  var init = function(event) {
+    if( 'undefined' !== typeof(event) && event.target.nodeName === "SCRIPT") start()
+  }
+  var js, ref = d.getElementsByTagName('script')[0]
+  js = d.createElement('script'); js.async = true
+  js.src = '//connect.soundcloud.com/sdk.js'
+  js.onload = init; ref.parentNode.insertBefore(js, ref)
+}(document))
